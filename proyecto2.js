4cd5971df88d43b3d37f888d732bc558e4ba01ae
ivans <svarchivan80@gmail.com>
2023-03-16 19:57:12 -0300

    cambio final

--- proyecto2.js
+++ proyecto2.js
@@ -3,10 +3,7 @@ class Provincias {
     this.provincia = provincia;
     this.distancia = distancia;
     this.precio = precio;
-    this.precioConIva;
-    this.aplicarIva = function () {
-        this.precioConIva = this.precio * 1.21
-    }
+
 }
 }
 
@@ -17,19 +14,37 @@ const neuquen = new Provincias("Neuquen",1000,600)
 const arrayProvincias = [mendoza, cordoba, neuquen];
 
 
-function saludar() {
-    alert("Bienvenido a Distribuidora La ibero")
-    let nombre = prompt("ingrese su nombre para seguir")
-    while(nombre === ""){
-        nombre = prompt("ingrese su nombre para seguir")
-    }
-
-let edad = parseInt(prompt("Ingrese su Edad"))
-    while(edad === ""){
-        edad = prompt("ingrese su nombre para seguir")
-    }
+   
+    function saludar() {
+        alert("Bienvenido a Distribuidora La ibero")
+        let nombre = prompt("ingrese su nombre para seguir")
+        while(nombre === ""){
+            nombre = prompt("ingrese su nombre para seguir")
+        }
+    
+        let apellido = prompt("ingrese su apellido para seguir")
+        while(apellido === ""){
+            apellido = prompt("ingrese su apellido para seguir")
+        }
+    
+    let edad = parseInt(prompt("Ingrese su Edad"))
+        while (edad === "" || edad< 18){
+            edad = prompt("ingrese su edad para seguir")
+        
+        }
+        
+        let email= parseInt(prompt("Ingrese su Email"))
+        while(email === ""){
+            email = prompt("ingrese su mail para seguir")
+        }
+    
+    
+        alert(`Bienvenidx   ${nombre} + ${apellido} `);
     
 }
+
+  
+
 const contenedorProductos = document.getElementById("contenedorProductos");
 arrayProvincias.forEach( producto => {
     const div = document.createElement("div");
@@ -39,27 +54,56 @@ arrayProvincias.forEach( producto => {
     contenedorProductos.appendChild(div);
 })
 
-  function cobrarAlCliente(producto){
-    let montoCliente = parseInt(prompt("Ingrese Monto"));
-    let precioConIva = producto.precio * 1.21;
-  
-    
-    if (montoCliente >= precioConIva) {
-        alert("Gracias por su compra!");
-        alert("Su vuelto es" + suVuelto);
-    } else {
-        alert("Lo siento, no le alcanza para comprar este producto.");
-    }
-}
-  
 
 
 
 function mostrarProvincias(){
-    let provSeleccionada= parseInt(prompt("En que provincia realizaremos el envio ?\n1)Mendoza\n2)Nequen\n3)Cordoba"))
+  let provSeleccionada= parseInt(prompt("En que provincia realizaremos el envio ?\n1)Mendoza\n2)Nequen\n3)Cordoba"))
 
-    while(provSeleccionada < 1 || provSeleccionada > 3){
-        provSeleccionada = parseInt(prompt("En que provincia realizaremos el envio ?\n1)Mendoza\n2)Nequen\n3)Cordoba"))
-    mostrarDetalles(provSeleccionada)
+   while(provSeleccionada < 1 || provSeleccionada > 3){
+       provSeleccionada = parseInt(prompt("En que provincia realizaremos el envio ?\n1)Mendoza\n2)Nequen\n3)Cordoba"))
+  
+}
+mostrarDetalles(provSeleccionada);
+}
+
+function mostrarDetalles(provSeleccionada){
+    if(provSeleccionada === 1){
+        console.log(`Usted selecciono : ${mendoza.provincia}`);
+        console.log("Debe abonar: $  "  +mendoza.precio); 
+
+    }
+    else if(provSeleccionada=== 2){
+        console.log(`Usted selecciono : ${cordoba.provincia}`);
+        console.log("Debe abonar: $  "  +cordoba.precio); 
+}
+else if(provSeleccionada=== 3){
+    console.log(`Usted selecciono : ${cordoba.provincia}`);
+    console.log("Debe abonar: $  "  +cordoba.precio); 
 }
 }
+  
+    
+const agregarProvinciaSiNoExiste = (provincia) => {
+    const existe = arrayProvincias.some(p => p.provincia === provincia);
+    if (!existe) {
+      const nuevaProvincia = new Provincias(provincia, 0, 0);
+      arrayProvincias.push(nuevaProvincia);
+         
+    const valores = arrayProvincias.map(p => `${p.provincia} (${p.precio}, ${p.distancia})`).join(', ');
+    console.log(`Valores actuales: ${valores}`);
+      alert("En 48 hs nos contactaremos para enviarle los valores a su localidad, Muchas gracias")
+      alert("si su ciudad ya se encontraba, desestime este mensaje")
+    }
+  }
+
+
+
+  
+
+saludar();
+mostrarProvincias();
+const nombreProvincia = prompt("Ingrese el nombre de la provincia a agregar en caso que no vea la suya:");
+agregarProvinciaSiNoExiste(nombreProvincia);
+
+
